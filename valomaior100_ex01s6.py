367c7fb6eee747c65e32c190fcfbe09ea5a6d37e
paolithiago <54008103+paolithiago@users.noreply.github.com>
2020-06-17 10:40:06 -0300

    Create valomaior100_ex01s6.py

---
+++ valomaior100_ex01s6.py
@@ -0,0 +1,14 @@
+"""Receber uma variavel numerica e verificar se ela e maior que 100, se nao for, imprimir o numero
+0. Estruturas condicionais."""
+
+#Entrada 
+Valor=int(input("Digite o valor:"))
+#Processamento
+if Valor >100:
+    print(Valor)
+else:
+    print(0)
+    
+     
+     
+
